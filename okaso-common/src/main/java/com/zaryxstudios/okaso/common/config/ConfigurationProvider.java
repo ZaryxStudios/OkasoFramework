396b29fdbb8eb2570f49a9e3f53da5f2c34a69b5
EscanerGang <escanergangog@gmail.com>
2026-07-21 17:38:01 +0200

    refactor(config): remove unused configuration classes
    - Removed unused ConfigurationProvider and ConfigurationSection interfaces
    - Deleted ScoreboardManager interface due to lack of usage
    - Simplified codebase by eliminating unnecessary classes
    - Improved maintainability by reducing dead code
    - Reduced potential for future conflicts by eliminating unused dependencies

--- okaso-common/src/main/java/com/zaryxstudios/okaso/common/config/ConfigurationProvider.java
+++
@@ -1,8 +0,0 @@
-package com.zaryxstudios.okaso.common.config;
-
-import java.io.File;
-
-public interface OkasoConfigurationProvider {
-    OkasoConfigurationSection load(File file);
-    void save(OkasoConfigurationSection section, File file);
-}
